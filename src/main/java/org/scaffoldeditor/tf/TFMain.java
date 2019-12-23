fff71813b775611f249ac157008e9446571b36ed
Sam54123 <thesam54123@gmail.com>
2019-12-22 21:43:21 -0800

    setup Bukkit plugin

---
+++ src/main/java/org/scaffoldeditor/tf/TFMain.java
@@ -0,0 +1,16 @@
+package org.scaffoldeditor.tf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TFMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+    
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
